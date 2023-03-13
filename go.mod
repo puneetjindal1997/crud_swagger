6538da26a46208813bba110468323fc723675211
puneetjindal1997 <puneetjindal1997@gmail.com>
2023-03-13 15:24:14 +0530

    jwt token updated

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
 	github.com/swaggo/files v1.0.0
 	github.com/swaggo/gin-swagger v1.5.3
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	docs "goguru/docs"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
@@ -25,6 +28,8 @@ import (
 
 // swagger=>
 // what is swagger?
+
+// JWT?
 
 type Data struct {
 	ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty" `
@@ -92,6 +97,105 @@ func init() {
 	connectDb()
 }
 
+type jwtWrapper struct {
+	SecretKey       string
+	Issuer          string
+	ExpirationHours int64
+}
+
+// JwtClaim adds email as a claim to the token
+type JwtClaim struct {
+	Email string
+	jwt.StandardClaims
+}
+
+// GenerateToken generates a jwt token
+func (j *jwtWrapper) GenerateToken(email string) (signedToken string, err error) {
+	claims := &JwtClaim{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(j.ExpirationHours)).Unix(),
+			Issuer:    j.Issuer,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err = token.SignedString([]byte(j.SecretKey))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// ValidateToken validates the jwt token
+func (j *jwtWrapper) ValidateToken(signedToken string) (claims *JwtClaim, err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JwtClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(j.SecretKey), nil
+		},
+	)
+
+	if err != nil {
+		return
+	}
+
+	claims, ok := token.Claims.(*JwtClaim)
+	if !ok {
+		err = errors.New("Couldn't parse claims")
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		err = errors.New("JWT is expired")
+		return
+	}
+
+	return
+
+}
+
+// Authz validates token and authorizes users
+func Authz() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("Authorization")
+		if clientToken == "" {
+			c.JSON(403, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+
+		extractedToken := strings.Split(clientToken, "Bearer ")
+
+		if len(extractedToken) == 2 {
+			clientToken = strings.TrimSpace(extractedToken[1])
+		} else {
+			c.JSON(400, "Incorrect Format of Authorization Token")
+			c.Abort()
+			return
+		}
+
+		jwtWrapper1 := jwtWrapper{
+			SecretKey: "esfsdfkpskodkf234234243243",
+			Issuer:    "admin",
+		}
+
+		claims, err := jwtWrapper1.ValidateToken(clientToken)
+		if err != nil {
+			c.JSON(401, err.Error())
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+
+		c.Next()
+
+	}
+}
+
 func main() {
 	r := gin.Default()
 
@@ -108,15 +212,32 @@ func main() {
 	r.NoRoute(func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/swagger/index.html")
 	})
-	r.POST("/data", insertData)
 	r.GET("/data1", getAll)
+	r.GET("/token", getToken)
+	r.Use(Authz())
+	r.POST("/data", insertData)
 	r.DELETE("/data", deleteData)
 	r.PUT("/data", updateData)
 	r.Run(":9090")
 
 }
 
+func getToken(c *gin.Context) {
+	jwtWrapper1 := jwtWrapper{
+		SecretKey:       "esfsdfkpskodkf234234243243",
+		Issuer:          "admin",
+		ExpirationHours: 48,
+	}
+	signedToken, jwtErr := jwtWrapper1.GenerateToken("goguru@gmail.com")
+	if jwtErr != nil {
+		fmt.Println(jwtErr)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"error": false, "message": "success", "token": signedToken})
+}
+
 func insertData(c *gin.Context) {
+	fmt.Println(c.Get("email"))
 	var d Data
 	err := c.BindJSON(&d)
 
